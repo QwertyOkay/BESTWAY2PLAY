abdd4afcc6bf11f32b520447ed8b88c4eb0537c1
Alexandr Kotenko <60352683+QwertyOkay@users.noreply.github.com>
2022-08-22 18:26:16 +0300

    update

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+(() => {
+    const refs = {
+      openModalBtn6: document.querySelector('[data-modal-open6]'),
+      closeModalBtn6: document.querySelector('[data-modal-close6]'),
+
+      modal6: document.querySelector('[data-modal6]'),
+      };
+  
+    refs.openModalBtn6.addEventListener('click', toggleModal6);
+    refs.closeModalBtn6.addEventListener('click', toggleModal6);
+      
+    function toggleModal6() {
+    refs.modal6.classList.toggle('is-hidden6');
+    }
+    
+    })();
+  
